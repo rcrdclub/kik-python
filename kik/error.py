45ff7d9e317a3aa32c8e059ed4a0bc49c818e28f
Shums Kassam <skassam21@gmail.com>
2017-03-20 11:46:15 -0400

    added some documentation

--- kik/error.py
+++ kik/error.py
@@ -2,6 +2,13 @@ class KikError(Exception):
     """
     Exception raised by all API errors.
     The exception message is set to the server's response.
+
+    :param message: The error message
+    :type message: str
+    :param status_code: Status code returned by the API call
+    :type status_code: int
+    :param content: Content returned by the API call
+    :type content: string
     """
     def __init__(self, message, status_code, content=None):
         super(KikError, self).__init__(message)
